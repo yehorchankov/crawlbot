7bbfc889808128cfa6293a10c02a2c9a6e42bfe0
yehor_chankov <egor.chankoff@gmail.com>
2016-11-21 00:49:25 +0200

    Add: HttpDownloader

---
+++ src/CrawlBot.Core/Concrete/HttpDownloader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrawlBot.Core.Abstract;
+using CrawlBot.Core.Models;
+
+namespace CrawlBot.Core.Concrete
+{
+    public class HttpDownloader : IWebDownloader
+    {
+        public HttpContext GetContent(HttpContext context)
+        {
+            return null;
+        }
+    }
+}
